fb4f9689e09e69262231fc8dbff9c0002309105d
Angelo Keirsebilck <angelo.keirsebilck1@telenet.be>
2022-01-30 19:11:02 +0100

    Add login, register, auth

---
+++ controllers/jobs.js
@@ -0,0 +1,21 @@
+const getAllJobs = async (req, res) => {
+  res.send('Get all jobs');
+};
+
+const getJob = async (req, res) => {
+  res.send('Get job');
+};
+
+const createJob = async (req, res) => {
+  res.send('createJob');
+};
+
+const updateJob = async (req, res) => {
+  res.send('updateJob');
+};
+
+const deleteJob = async (req, res) => {
+  res.send('deleteJob');
+};
+
+module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
